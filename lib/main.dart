617e7404d4636879186d3ee869e4b7cb464c7e84
zwelhtet4za <zwel@za.com.mm>
2023-09-03 12:40:37 +0630

    init

---
+++ lib/main.dart
@@ -0,0 +1,214 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:blue_print_pos/blue_print_pos.dart';
+import 'package:blue_print_pos/models/models.dart';
+import 'package:blue_print_pos/receipt/receipt_section_text.dart';
+import 'package:blue_print_pos/receipt/receipt_text_enum.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+Future<String> networkImageToBase64(String imageUrl) async {
+  http.Response response = await http.get(Uri.parse(imageUrl));
+  final bytes = response.bodyBytes;
+
+  debugPrint('converted image =>>> ${base64Encode(bytes)}');
+  return base64Encode(bytes);
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final BluePrintPos _bluePrintPos = BluePrintPos.instance;
+  List<BlueDevice> _blueDevices = <BlueDevice>[];
+  BlueDevice? _selectedDevice;
+  bool _isLoading = false;
+  int _loadingAtIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Blue Print Pos'),
+        ),
+        body: SafeArea(
+          child: _isLoading && _blueDevices.isEmpty
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                  ),
+                )
+              : _blueDevices.isNotEmpty
+                  ? SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: List<Widget>.generate(_blueDevices.length, (int index) {
+                              return Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: GestureDetector(
+                                      onTap: _blueDevices[index].address == (_selectedDevice?.address ?? '')
+                                          ? _onDisconnectDevice
+                                          : () => _onSelectDevice(index),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              _blueDevices[index].name,
+                                              style: TextStyle(
+                                                color: _selectedDevice?.address == _blueDevices[index].address ? Colors.blue : Colors.black,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            Text(
+                                              _blueDevices[index].address,
+                                              style: TextStyle(
+                                                color:
+                                                    _selectedDevice?.address == _blueDevices[index].address ? Colors.blueGrey : Colors.grey,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  if (_loadingAtIndex == index && _isLoading)
+                                    Container(
+                                      height: 24.0,
+                                      width: 24.0,
+                                      margin: const EdgeInsets.only(right: 8.0),
+                                      child: const CircularProgressIndicator(
+                                        valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.blue,
+                                        ),
+                                      ),
+                                    ),
+                                  if (!_isLoading && _blueDevices[index].address == (_selectedDevice?.address ?? ''))
+                                    TextButton(
+                                      onPressed: _onPrintReceipt,
+                                      child: Container(
+                                        color: _selectedDevice == null ? Colors.grey : Colors.blue,
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: const Text(
+                                          'Test Print',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                      style: ButtonStyle(
+                                        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                                          (Set<MaterialState> states) {
+                                            if (states.contains(MaterialState.pressed)) {
+                                              return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                                            }
+                                            return Theme.of(context).primaryColor;
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              );
+                            }),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Text(
+                            'Scan bluetooth device',
+                            style: TextStyle(fontSize: 24, color: Colors.blue),
+                          ),
+                          Text(
+                            'Press button scan',
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _isLoading ? null : _onScanPressed,
+          child: const Icon(Icons.search),
+          backgroundColor: _isLoading ? Colors.grey : Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onScanPressed() async {
+    setState(() => _isLoading = true);
+    _bluePrintPos.scan().then((List<BlueDevice> devices) {
+      if (devices.isNotEmpty) {
+        setState(() {
+          _blueDevices = devices;
+          _isLoading = false;
+        });
+      } else {
+        setState(() => _isLoading = false);
+      }
+    });
+  }
+
+  void _onDisconnectDevice() {
+    _bluePrintPos.disconnect().then((ConnectionStatus status) {
+      if (status == ConnectionStatus.disconnected) {
+        setState(() {
+          _selectedDevice = null;
+        });
+      }
+    });
+  }
+
+  void _onSelectDevice(int index) {
+    setState(() {
+      _isLoading = true;
+      _loadingAtIndex = index;
+    });
+    final BlueDevice blueDevice = _blueDevices[index];
+    _bluePrintPos.connect(blueDevice).then((ConnectionStatus status) {
+      if (status == ConnectionStatus.connected) {
+        setState(() => _selectedDevice = blueDevice);
+      } else if (status == ConnectionStatus.timeout) {
+        _onDisconnectDevice();
+      } else {
+        print('$runtimeType - something wrong');
+      }
+      setState(() => _isLoading = false);
+    });
+  }
+
+  Future<void> _onPrintReceipt() async {
+    /// Example for Print Image
+    final ByteData logoBytes = await rootBundle.load(
+      'assets/logo.jpg',
+    );
+
+    /// Example for Print Text
+    final ReceiptSectionText receiptText = ReceiptSectionText();
+
+    receiptText.addText(text: 'Hello World', is80: true, alignment: ReceiptAlignment.center, size: ReceiptTextSize.normal);
+
+    receiptText.addText(text: 'This is testing ', is80: true, alignment: ReceiptAlignment.center, size: ReceiptTextSize.normal);
+
+    // // receiptSecondText.addSpacer();
+    await _bluePrintPos.printReceiptText(receiptText, is80: false);
+  }
+}
